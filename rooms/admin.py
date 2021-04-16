3cffbc5479a08510b2fe4bc8c0e1447e319def6d
kanakan12 <69573636+kanakan12@users.noreply.github.com>
2021-04-16 14:01:39 +0900

    Updated admin panel

--- rooms/admin.py
+++ rooms/admin.py
@@ -6,6 +6,11 @@ class ItemAdmin(admin.ModelAdmin):
 
     """ Item Admin Definition """
 
+    list_display = ("name", "used_by")
+
+    def used_by(self, obj):
+        return obj.rooms.count()
+
     pass
 
 # Register your models here.
@@ -14,7 +19,50 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields":("name", "description", "country", "address", "price")}
+        ),
+        (
+            "Times",
+            {"fields":("check_in", "check_out", "instant_book")}
+        ),
+        (
+            "Spaces",
+            {"fields":("guests", "beds", "bedrooms", "baths")}
+        ),
+        (
+            "More About the Space",
+            {
+                "classes":("collapse",),
+                "fields":("amenities", "facilities", "house_rules")
+            }
+        ),
+        (
+            "Last Details",
+            {"fields":("host",)}
+        )
+    )
+
+    list_display = ("name", "country", "city", "host",
+                   "price", "guests", "beds", "bedrooms", 
+                   "baths", "check_in", "check_out", 
+                   "instant_book", "count_amenities", "count_photos")
+
+    ordering = ("price",)
+
+    list_filter = ("instant_book", "city", "room_type", "amenities", "facilities", "house_rules", "country")
+
+    search_fields = ("city", "host__username")
+
+    filter_horizontal = ("amenities", "facilities", "house_rules")
+
+    def count_amenities(self, obj):
+        return obj.amenities.count()
+
+    def count_photos(self, obj):
+        return obj.photos.count()
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
