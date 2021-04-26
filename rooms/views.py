5059ee38c8eb39f026b05f3c4a4cbabdd6322493
kanakan12 <69573636+kanakan12@users.noreply.github.com>
2021-04-26 22:55:33 +0900

    Added RoomDetailView

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,10 @@
 # from math import ceil
 # from django.shortcuts import render, redirect
 # from django.core.paginator import Paginator, EmptyPage
-from django.utils import timezone
-from django.views.generic import ListView
+# from django.utils import timezone
+from django.views.generic import ListView, DetailView
+# from django.http import Http404
+# from django.shortcuts import render
 from . import models
 # Create your views here.
 
@@ -16,11 +18,24 @@ class HomeView(ListView):
     ordering = "created"
     context_object_name = "rooms"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        now = timezone.now()
-        context["now"] = now
-        return context
+class RoomDetail(DetailView):
+
+    """ RoomDetail Definition """
+
+    model = models.Room
+
+# def room_detail(request, pk):
+#     try:
+#         room = models.Room.objects.get(pk=pk)
+#         return render(request, "rooms/detail.html", {"room":room})
+#     except models.Room.DoesNotExist:
+#         raise Http404()
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     now = timezone.now()
+    #     context["now"] = now
+    #     return context
 
 # def all_rooms(request):
 #     page = request.GET.get("page", 1)
